f529a68343ca2c298876531c0145a239f1c7d2f6
Aniket M <mhatreaniket121@gmail.com>
2022-03-17 16:02:49 +0530

    Lecture 12 | Implemented send functionality

--- src/main/java/io/javabrains/inbox/controllers/ComposeController.java
+++ src/main/java/io/javabrains/inbox/controllers/ComposeController.java
@@ -1,15 +1,22 @@
 package io.javabrains.inbox.controllers;
 
+import io.javabrains.inbox.email.Email;
+import io.javabrains.inbox.email.EmailService;
 import io.javabrains.inbox.folders.FolderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.MultiValueMap;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -21,6 +28,8 @@ public class ComposeController {
     private InboxHelper helper;
     @Autowired
     private FolderService folderService;
+    @Autowired
+    private EmailService emailService;
 
     @GetMapping(value = "/compose")
     public String composeEmail(@AuthenticationPrincipal OAuth2User principal, Model model, @RequestParam(required = false) String to){
@@ -36,13 +45,37 @@ public class ComposeController {
 
         // Adding recipients list for reply and replyAll
         if(StringUtils.hasText(to)) {
-            List<String> replyToIds = Arrays.stream(to.split(","))
-                    .map(StringUtils::trimWhitespace)
-                    .filter(StringUtils::hasText)
-                    .distinct()
-                    .collect(Collectors.toList());
+            List<String> replyToIds = splitToIds(to);
             model.addAttribute("replyToIds", String.join(", ", replyToIds));
         }
         return "compose-page";
     }
+
+    /*
+        Split comma separated ids to List of Id's
+     */
+    private List<String> splitToIds(String to) {
+        if(!StringUtils.hasText(to)){
+            return new ArrayList<>();
+        }
+        return Arrays.stream(to.split(","))
+                .map(StringUtils::trimWhitespace)
+                .filter(StringUtils::hasText)
+                .distinct()
+                .collect(Collectors.toList());
+    }
+
+    @PostMapping(value = "/send")
+    public ModelAndView sendEmail(@AuthenticationPrincipal OAuth2User principal, @RequestBody MultiValueMap<String, String> formData){
+        if(principal==null || !StringUtils.hasText(principal.getAttribute("login"))){
+            return new ModelAndView("redirect:/");
+        }
+        String userId = principal.getAttribute("login");
+        String body = formData.getFirst("body");
+        String subject = formData.getFirst("subject");
+        List<String> ids = splitToIds(formData.getFirst("toIds"));
+        emailService.sendEmail(userId, ids, subject, body);
+        return new ModelAndView("redirect:/");
+    }
+
 }
--- src/main/java/io/javabrains/inbox/email/EmailService.java
+++ src/main/java/io/javabrains/inbox/email/EmailService.java
@@ -1,8 +1,13 @@
 package io.javabrains.inbox.email;
 
+import com.datastax.oss.driver.api.core.uuid.Uuids;
+import io.javabrains.inbox.emaillist.EmailListItem;
+import io.javabrains.inbox.emaillist.EmailListItemKey;
+import io.javabrains.inbox.emaillist.EmailListItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -10,9 +15,39 @@ import java.util.UUID;
 public class EmailService {
     @Autowired
     private EmailRepository emailRepository;
+    @Autowired
+    private EmailListItemRepository emailListItemRepository;
 
     public Email findById(UUID id){
         Optional<Email> optionalEmail = emailRepository.findById(id);
         return optionalEmail.orElse(null);
+    }
+
+    public void sendEmail(String from, List<String> to, String subject, String body){
+        Email email = new Email();
+        email.setTo(to);
+        email.setFrom(from);
+        email.setSubject(subject);
+        email.setBody(body);
+        email.setId(Uuids.timeBased());
+        emailRepository.save(email);
+
+        to.forEach(id -> {
+            createEmailListItem(to, email, id, "Inbox");
+        });
+        createEmailListItem(to, email, from, "Sent Items");
+    }
+
+    private void createEmailListItem(List<String> to, Email email, String id, String label) {
+        EmailListItemKey key = new EmailListItemKey();
+        EmailListItem emailListItem = new EmailListItem();
+        key.setTimeUUID(email.getId());
+        key.setLabel(label);
+        key.setUserId(id);
+        emailListItem.setKey(key);
+        emailListItem.setSubject(email.getSubject());
+        emailListItem.setUnread(true);
+        emailListItem.setTo(to);
+        emailListItemRepository.save(emailListItem);
     }
 }
